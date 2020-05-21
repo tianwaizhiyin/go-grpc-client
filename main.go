f1b4496b36eefdc1456920f1fd6385aea6fe2f90
893335792@qq.com <893335792@qq.com>
2020-05-21 19:29:59 +0800

    202005211929

--- main.go
+++ main.go
@@ -1,6 +1,31 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"github.com/tianwaizhiyin/go-grpc-client.git/services"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"log"
+)
+
 func main()  {
-	conn, err := grpc.Dial(":8080")
+	creds, err := credentials.NewClientTLSFromFile("keys/server.crt", "dongfangfuli.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn, err := grpc.Dial(":8080", grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	prodClient := services.NewProdServiceClient(conn)
+	prodRes, err := prodClient.GetProdStock(context.Background(), &services.ProdRequest{ProdId:12})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(prodRes.ProdStock)
 
 }
