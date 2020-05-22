2371a9614632217bc5773a47c7262cca963187e9
893335792@qq.com <893335792@qq.com>
2020-05-22 16:32:19 +0800

    2020-05221632

--- main.go
+++ main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"github.com/tianwaizhiyin/go-grpc-client.git/services"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"io/ioutil"
 	"log"
 )
 
 func main()  {
-	creds, err := credentials.NewClientTLSFromFile("keys/server.crt", "dongfangfuli.com")
-	if err != nil {
-		log.Fatal(err)
-	}
+	//creds, err := credentials.NewClientTLSFromFile("keys/server.crt", "dongfangfuli.com")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	//tls证书认证
+	cert, _ := tls.LoadX509KeyPair("cert/client.pem", "cert/client.key")
+	certPool := x509.NewCertPool()
+	ca, _ := ioutil.ReadFile("cert/ca.pem")
+	certPool.AppendCertsFromPEM(ca)
+
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert}, //客户端证书
+		ServerName:    "localhost",
+		RootCAs:       certPool,
+	})
+
 
 	conn, err := grpc.Dial(":8080", grpc.WithTransportCredentials(creds))
 	if err != nil {
@@ -27,5 +43,4 @@ func main()  {
 		log.Fatal(err)
 	}
 	fmt.Println(prodRes.ProdStock)
-
 }
